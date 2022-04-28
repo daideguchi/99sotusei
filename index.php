317347dd571621bdaed206c327454edbb4fbb8f1
daideguchi <dd.1107.11107@gmail.com>
2022-04-29 00:44:22 +0900

    34

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/heroes/">
   <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
-      <link rel="icon" href="favicon.ico">
+  <link rel="icon" href="favicon.ico">
 
    <!-- Bootstrap core CSS -->
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
