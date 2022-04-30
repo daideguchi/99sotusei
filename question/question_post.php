dcf2a03c089041bb43a6eb2d5c8fcf40cc57c89e
daideguchi <dd.1107.11107@gmail.com>
2022-04-30 13:30:35 +0900

    79

--- question/question_post.php
+++ question/question_post.php
@@ -58,7 +58,7 @@ check_session_id();
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a href="../toppage.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4"><img id="nav_l" src="images/IMG_5507.PNG" alt="アイコン" style="height:50px;">Chalk up</span>
+            <span class="fs-4"><img id="nav_l" src="../images/IMG_5507.PNG" alt="アイコン" style="height:50px;">Chalk up</span>
           </a>
 
           <ul class="nav nav-pills">
