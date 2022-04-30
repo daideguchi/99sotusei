1b069e6cac563a0b7f8483f8bbc6b359cc26a57f
daideguchi <dd.1107.11107@gmail.com>
2022-04-30 11:10:07 +0900

    73

--- post/post.check.php
+++ post/post.check.php
@@ -66,59 +66,14 @@ if(is_uploaded_file($tmp_path)){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>file_upload</title>
-      <!-- <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-earthly.css" type="text/css"> -->
-
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-      <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
-
-     <!-- Bootstrap core CSS -->
- <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<style>
-  .bd-placeholder-img {
-    font-size: 1.125rem;
-    text-anchor: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    user-select: none;
-  }
-
-  @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-      font-size: 3.5rem;
-    }
-  }
-</style>
-
-
-<!-- Custom styles for this template -->
-<link href="headers.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-earthly.css" type="text/css">
 
 </head>
 
-<body class="landing is-preload">
-
-     <!-- ヘッダー、ナビゲーションバー、選択時の色を変える -->
-     <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="../toppage.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4"><img id="nav_l" src="../images/IMG_5507.PNG" alt="アイコン">Chalk up</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="../mypage.php" class="nav-link" aria-current="page">マイページ</a></li>
-            <li class="nav-item"><a href="../search.php" class="nav-link">探す</a></li>
-            <li class="nav-item"><a href="post/post.php" class="nav-link">書く</a></li>
-            <li class="nav-item"><a href="../question.php" class="nav-link">質問する</a></li>
-            <li class="nav-item"><a href="../background/todo_logout.php" class="nav-link">ログアウト</a></li>
-          </ul>
-        </header>
-      </div>
+<body>
     
-  <main>
 
-  <h1 class="account">投稿内容の確認</h1>
+  <h1>投稿内容の確認</h1>
     <br>
 
 
@@ -145,7 +100,6 @@ if(is_uploaded_file($tmp_path)){
 <input type="hidden" name="text" value="<?php echo "{$text}" ?>">
 <input type="hidden" name="thumbnail" value="<?php echo "{$save_path}" ?>">
 
-<br>
 <button>投稿</button>
 
   </form>
@@ -153,68 +107,11 @@ if(is_uploaded_file($tmp_path)){
 <!-- <input type="button" value="修正" src="post.php"> -->
 <a href="post.php">修正</a>
 
-</main>
 
 </body>
 
     <style>
-      main{
-
-        display: flex;
-        /* justify-content: center;
-        text-align: center; */
-        align-items: center;
-        flex-direction: column;
-        padding-left: 350px;
-        padding-right: 350px;
-}
-
-.account{
-    font-size: 30px;
-    display: flex;
-    text-align: center;
-  }
         #thumbnail{
             width: 200px;
-        }
-
-        #output li {
-            background-color: rgb(231, 235, 218);
-            list-style-type: none
-
-        }
-
-        #send{
-            width: 150px;
-        }
-
-        #nyuuryoku{
-            margin-top: 30px;
-        }
-
-        #setumei{
-            font-size: 10px;
-        }
-
-        #top{
-            /* width: 100%; */
-            display: flex;
-          justify-content: space-between;
-        }
-
-        #top h1{
-            width: 100%;
-        }
-
-        #top img{
-            width: 30%;
-        }
-
-        #showPic{
-            width: 200px;
-        }
-
-        #nav_l{
-            height:50px;
         }
     </style>
--- post/post.php
+++ post/post.php
@@ -14,7 +14,7 @@ check_session_id();
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!-- <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-earthly.css" type="text/css"> -->
+    <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-earthly.css" type="text/css">
     <link rel="stylesheet" type="text/css" href="../editer_tool/jquery.cleditor.css" />
 
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
@@ -58,17 +58,17 @@ check_session_id();
          <!-- ヘッダー、ナビゲーションバー、選択時の色を変える -->
       <div class="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="../toppage.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <a href="toppage.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4"><img id="nav_l" src="../images/IMG_5507.PNG" alt="アイコン">Chalk up</span>
+            <span class="fs-4"><img id="nav_l" src="../images/IMG_5507.PNG" alt="アイコン" style="height:50px;">Chalk up</span>
           </a>
 
           <ul class="nav nav-pills">
-            <li class="nav-item"><a href="../mypage.php" class="nav-link" aria-current="page">マイページ</a></li>
-            <li class="nav-item"><a href="../search.php" class="nav-link">探す</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">書く</a></li>
+            <li class="nav-item"><a href="./mypage.php" class="nav-link" aria-current="page">マイページ</a></li>
+            <li class="nav-item"><a href="./search.php" class="nav-link">探す</a></li>
+            <li class="nav-item"><a href="./post/post.php" class="nav-link">書く</a></li>
             <li class="nav-item"><a href="../question.php" class="nav-link">質問する</a></li>
-            <li class="nav-item"><a href="../background/todo_logout.php" class="nav-link">ログアウト</a></li>
+            <li class="nav-item"><a href="./background/todo_logout.php" class="nav-link">ログアウト</a></li>
           </ul>
         </header>
       </div>
@@ -88,7 +88,8 @@ check_session_id();
                     <div>タイトル: <input type="text" name="title" style="
                         width: 622px;
                         padding-left: 10px;
-                        padding-right: 10px;"></div>
+                        padding-right: 10px;
+                    "></div>
                     <br />
 
                     <div>本文: </div>
@@ -166,9 +167,5 @@ check_session_id();
 
         #showPic{
             width: 200px;
-        }
-
-        #nav_l{
-            height:50px;
         }
     </style>
